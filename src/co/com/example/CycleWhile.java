ce1bc71613e2dd570fa8e7d875c0c38fa7bfb8a0
Bymarium <marianamejia.91619@gmail.com>
2022-12-14 21:05:24 -0500

    First commit

---
+++ src/co/com/example/CycleWhile.java
@@ -0,0 +1,12 @@
+package co.com.example;
+
+public class CycleWhile {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter < 3){
+            System.out.println("counter = " + counter);
+            counter++;
+        }
+        
+    }
+}
